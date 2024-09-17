ba02cab4fa0feddf06133c4153f08acf6f2f284e
Ulrich.Rohloff <Ulrich.Rohloff@aerosoft.de>
2024-09-17 15:55:45 +0200

    update

--- main.js
+++ main.js
@@ -3,9 +3,5 @@ const shareButton = document.getElementById("shareButton");
 
 
 shareButton.addEventListener("click", (e) => {
-    if (shareWindow.style.display === "none" || shareWindow.style.display === "") {
-        shareWindow.style.display = "flex";
-    } else {
-        shareWindow.style.display = "none";
-    }
+    shareWindow.classList.toggle("flex");
 });
